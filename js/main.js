17492a758a9c01e1a0ef4dfb9d3d349619fc9d4b
Jordan Johansen & charlesamoss <student@epicodus.com>
2015-09-10 14:41:18 -0700

    first test passes

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var wordOrder = function (phrase) {
+
+
+    return [];
+}
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe('wordOrder', function() {
+  it("input a string and output an array", function() {
+    expect(wordOrder("hello world")).to.be.an("array");
+  });
+
+});
