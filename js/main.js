5bfa18f9a07737634530dafa13b05cd59c2c502d
Jordan Johansen & charlesamoss <student@epicodus.com>
2015-09-10 16:27:58 -0700

    4th test passes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 var wordOrder = function (phrase) {
 
+    var wordArr = phrase.split(" ");
+    var counts = {};
 
-    return [];
+    for(var i = 0; i< wordArr.length; i++) {
+        var word = wordArr[i];
+
+        counts[word] = counts[word] ? counts[word]+1 : 1;
+    }
+
+    // var countArr = Object.keys(counts);
+    // var num = 0;
+
+
+    var finArr = [];
+    for (var key in counts) {
+        finArr.push([key, counts[key]]);
+    };
+    finArr.sort(function(a, b) {return b[1]-a[1]});
+    return finArr;
+
 }
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,16 @@ describe('wordOrder', function() {
     expect(wordOrder("hello world")).to.be.an("array");
   });
 
+ it("input a two unique word string and output an array with those words with keys", function() {
+    expect(wordOrder("hello world")).to.eql([["hello", 1], ["world", 1]]);
+});
+
+it("input a string with a repeating word and output an array of array ordered by word count ", function() {
+   expect(wordOrder("hello world world")).to.eql([["world", 2], ["hello", 1]]);
+});
+
+it("input a string with even more repeating words and output an array of array ordered by word count", function() {
+   expect(wordOrder("hello hello hello goodbye world world")).to.eql([["hello", 3], ["world", 2], ["goodbye", 1]]);
+});
+
 });
