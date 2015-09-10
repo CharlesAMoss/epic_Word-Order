69eb7e0da8e284560f22bb4f6f6e5914645d732d
Jordan Johansen & charlesamoss <student@epicodus.com>
2015-09-10 16:48:57 -0700

    Add order test, passes

--- js/main.js
+++ js/main.js
@@ -9,15 +9,20 @@ var wordOrder = function (phrase) {
         counts[word] = counts[word] ? counts[word]+1 : 1;
     }
 
-    // var countArr = Object.keys(counts);
-    // var num = 0;
-
-
     var finArr = [];
     for (var key in counts) {
         finArr.push([key, counts[key]]);
     };
     finArr.sort(function(a, b) {return b[1]-a[1]});
+    debugger;
     return finArr;
-
 }
+
+// Potential display formatting function
+// var displayList = function(finArr) {
+//     var string = "";
+//     finArr.forEach(function(pair) {
+//         string += pair[0] : pair[1];
+//     });
+//     return string;
+// }
